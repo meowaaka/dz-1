9ce9b0e71fc56fe6b84399e261a3d67c50fd58b2
Dubrovina Eva <meowaaka@gmail.com>
2026-05-10 16:07:19 +0300

    “new”

--- Source.cpp
+++ Source.cpp
@@ -1,28 +1,54 @@
 #include <iostream>
 using namespace std;
 
-#include "book.h"
-#include "Header.h"
-
 int main() {
-    uint SIZE;
-    cout << "Enter number of books: ";
-    cin >> SIZE;
+    const char* PATH_TO_FILE = "test.txt";
+    const char* MODE = "a"; //"w" - write, "a" - append, "r" - read
+    FILE* file;
+    int code = fopen_s(&file, PATH_TO_FILE, MODE); // відкрити файл
+    unsigned int num = 0;
+    char ch;
+    do {
+        cout << "Enter char (q - quit) = ";
+        ch = getchar();
+        if (ch >= '0' && ch <= '9') {
+            num++;
+        }
 
-    Book* books = new Book[SIZE];
+        if (ch == 'q') {
+            break;
+        }
+        fputc(ch, file);
+        cin.ignore();
 
-    for (uint i = 0; i < SIZE; i++)
-    {
-        cout << "\n--- Create book " << i + 1 << " ---\n";
-        books[i] = createBook();
+    } while (true);
+    cout << "Digits entered = " << num << endl;
+
+    if (code == 0) { //SUCCES
+        fclose(file); // закрити файл
+
+        // Тепер відкриваємо файл для читання
+        FILE* readFile;
+        fopen_s(&readFile, PATH_TO_FILE, "r");
+        if (readFile) {
+            unsigned int digits = 0, upper = 0, lower = 0;
+            char c;
+            while ((c = fgetc(readFile)) != EOF) {
+                if (c >= '0' && c <= '9') digits++;
+                else if (c >= 'A' && c <= 'Z') upper++;
+                else if (c >= 'a' && c <= 'z') lower++;
+            }
+            fclose(readFile);
+
+            cout << "In file:\n";
+            cout << "Digits = " << digits << endl;
+            cout << "Uppercase letters = " << upper << endl;
+            cout << "Lowercase letters = " << lower << endl;
+        }
     }
-
-    printBooks(books, SIZE);
-    searchBook(books, SIZE);
-
-    for (uint i = 0; i < SIZE; i++)
-        delete[] books[i].title;
-    delete[] books;
+    else { //ERROR
+        cout << code;
+    }
 
     return 0;
 }
