f99b24eb0dc49fd65e1b176478133d703526b6e2
Dubrovina Eva <meowaaka@gmail.com>
2026-05-31 22:42:24 +0300

    “new_hw3”

--- Source.cpp
+++ Source.cpp
@@ -1,82 +1,20 @@
 #include <iostream>
-
 using namespace std;
 
-const int N = 30;
+#define POWER(base, exp) (pow((base), (exp)))
 
-struct Player {
-    char* login = nullptr;
-    char* password = nullptr;
-    int level = 0;
-    float money = 0;
-};
+#define SQRT(num) (sqrt((num)))
 
-void show(const Player& pl) {
-    cout << "Login: " << pl.login
-        << " | Password: " << pl.password
-        << " | Level: " << pl.level
-        << " | Money: " << pl.money << endl;
-}
+#define MULTIPLY3(a, b, c) ((a) * (b) * (c))
 
 int main() {
-    const char* PATH = "player.bin";
-    const char* MODE = "ab+";
+    double x = 2.0;
+    double y = 9.0;
+    int a = 2, b = 3, c = 4;
 
-    Player bogdan;
-    char l[N], p[N];
-    cout << "Enter login: ";
-    cin >> l;
-    cout << "Enter password: ";
-    cin >> p;
-    cout << "Enter level: ";
-    cin >> bogdan.level;
-    bogdan.money = 100;
+    cout << "2^3 = " << POWER(x, 3) << endl;
+    cout << "sqrt(9) = " << SQRT(y) << endl;
+    cout << "2 * 3 * 4 = " << MULTIPLY3(a, b, c) << endl;
 
-    bogdan.login = new char[strlen(l) + 1];
-    strcpy_s(bogdan.login, strlen(l) + 1, l);
-    bogdan.password = new char[strlen(p) + 1];
-    strcpy_s(bogdan.password, strlen(p) + 1, p);
-
-    FILE* f;
-    if (fopen_s(&f, PATH, MODE) == 0) {
-        fwrite(&bogdan, sizeof(Player), 1, f);
-
-        int sizeLogin = strlen(bogdan.login) + 1;
-        fwrite(&sizeLogin, sizeof(int), 1, f);
-        fwrite(bogdan.login, sizeLogin, 1, f);
-
-        int sizePwd = strlen(bogdan.password) + 1;
-        fwrite(&sizePwd, sizeof(int), 1, f);
-        fwrite(bogdan.password, sizePwd, 1, f);
-
-        fseek(f, 0, SEEK_SET);
-        while (!feof(f)) {
-            Player pl;
-            int sL, sP;
-            if (fread(&pl, sizeof(Player), 1, f) > 0) {
-                if (fread(&sL, sizeof(int), 1, f) > 0) {
-                    char* loginBuf = new char[sL];
-                    fread(loginBuf, sL, 1, f);
-                    pl.login = new char[sL];
-                    strcpy_s(pl.login, sL, loginBuf);
-                    delete[] loginBuf;
-                }
-                if (fread(&sP, sizeof(int), 1, f) > 0) {
-                    char* pwdBuf = new char[sP];
-                    fread(pwdBuf, sP, 1, f);
-                    pl.password = new char[sP];
-                    strcpy_s(pl.password, sP, pwdBuf);
-                    delete[] pwdBuf;
-                }
-                show(pl);
-                delete[] pl.login;
-                delete[] pl.password;
-            }
-        }
-        fclose(f);
-    }
-
-    delete[] bogdan.login;
-    delete[] bogdan.password;
     return 0;
 }
